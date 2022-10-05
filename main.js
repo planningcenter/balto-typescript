9b8cb8ad7aa1e0c8948ebef7cbebb1243ae26221
Kyle Mellander <kylemellander@gmail.com>
2022-10-05 08:22:57 -0700

    transition repo to be for typescript

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 const io = require('@actions/io')
-const semver = require('semver')
 const { easyExec, setOutput } = require('./utils')
-const { generateChangeRanges } = require('./git_utils')
 const CheckRun = require('./check_run')
 
 const {
   GITHUB_WORKSPACE,
-  INPUT_EXTENSIONS,
   INPUT_CONCLUSIONLEVEL,
-  INPUT_FAILURELEVEL
 } = process.env
 
 const event = require(process.env.GITHUB_EVENT_PATH)
-const checkName = 'ESLint'
+const checkName = 'TypeScript'
 
-let eslintVersionSevenOrGreater = null
-let linter = null
+let typeScriptCommand = null
 let yarnOutput = null
 
 async function getYarn () {
@@ -49,11 +44,11 @@ async function getPeerDependencies (error) {
   return versions
 }
 
-async function installEslintPackagesAsync () {
+async function installJSPackagesAsync () {
   const yarn = await getYarn()
 
   const versions = yarn.data.trees
-    .filter(p => p.name.match(/eslint/) || p.name.match(/prettier/))
+    .filter(p => p.name.match(/@types/) || p.name.match(/typescript/) || p.name.match(/tsc-silent/))
     .map(p => p.name)
 
   await io.mv('package.json', 'package.json-bak')
@@ -71,97 +66,21 @@ async function installEslintPackagesAsync () {
   }
 }
 
-async function setupEslintVersionAndLinter () {
-  const eslintVersion =
-    require(`${GITHUB_WORKSPACE}/node_modules/eslint/package.json`).version
-  eslintVersionSevenOrGreater = semver.gte(eslintVersion, '7.0.0')
-
-  const eslint = require(`${GITHUB_WORKSPACE}/node_modules/eslint`)
-  linter = eslintVersionSevenOrGreater
-    ? new eslint.ESLint()
-    : new eslint.CLIEngine()
-}
-
-function gatherReportForEslintSixOrLower (paths) {
-  return linter.executeOnFiles(paths)
-}
-
-async function gatherReportForEslintSevenOrGreater (paths) {
-  const report = await linter.lintFiles(paths)
-
-  return report.reduce(
-    ({ results, errorCount, warningCount }, result) => ({
-      results: [...results, result],
-      errorCount: errorCount + result.errorCount,
-      warningCount: warningCount + result.warningCount
-    }),
-    { results: [], errorCount: 0, warningCount: 0 }
-  )
-}
-
-async function runEslint () {
-  const compareSha = event.pull_request.base.sha
-
-  const { output } = await easyExec(
-    `git diff --name-only --diff-filter AM ${compareSha}`
-  )
-  const extensions = INPUT_EXTENSIONS.split(',')
-
-  const paths = output
-    .split('\n')
-    .filter(path => extensions.some(e => path.endsWith(`.${e}`)))
-
-  const report = eslintVersionSevenOrGreater
-    ? await gatherReportForEslintSevenOrGreater(paths)
-    : gatherReportForEslintSixOrLower(paths)
-
-  const { results, errorCount, warningCount } = report
-
-  const levels = ['', 'warning', 'failure']
-
-  const annotations = []
-
-  for (let resultsIndex = 0; resultsIndex < results.length; resultsIndex++) {
-    const result = results[resultsIndex]
-    const { filePath, messages } = result
-    const path = filePath.substring(GITHUB_WORKSPACE.length + 1)
-
-    if (await linter.isPathIgnored(path)) continue
-
-    const changeRanges = await generateChangeRanges(path, { compareSha })
-
-    for (
-      let messagesIndex = 0;
-      messagesIndex < messages.length;
-      messagesIndex++
-    ) {
-      const msg = messages[messagesIndex]
-
-      const { line, severity, ruleId, message } = msg
-
-      if (!changeRanges.some(r => r.doesInclude(line))) continue
-
-      const annotationLevel = levels[severity]
-      annotations.push({
-        path,
-        start_line: line,
-        end_line: line,
-        annotation_level: annotationLevel,
-        message: `[${ruleId}] ${message}`
-      })
-    }
+async function setupTypescriptCommand () {
+  try {
+    require(`${GITHUB_WORKSPACE}/node_modules/tsc-silent`)
+    require(`${GITHUB_WORKSPACE}/tsc-silent.config.js`)
+    typeScriptCommand = `${GITHUB_WORKSPACE}/node_modules/tsc-silent/bin/tsc-silent -p ${GITHUB_WORKSPACE}/tsconfig.json --suppressConfig ${GITHUB_WORKSPACE}/tsc-silent.config.js`
+  } catch(e) {
+    typeScriptCommand = `${GITHUB_WORKSPACE}/node_modules/typescript/bin/tsc`
   }
+}
 
-  const totalCount = INPUT_FAILURELEVEL === "warning" ? errorCount + warningCount : errorCount
+async function runTypeScriptCommand () {
+  const results = await easyExec(typeScriptCommand)
+  const conclusion = results.error ? INPUT_CONCLUSIONLEVEL : 'success'
 
-  return {
-    conclusion: totalCount > 0 ? INPUT_CONCLUSIONLEVEL : 'success',
-    output: {
-      title: checkName,
-      summary: `${errorCount} error(s), ${warningCount} warning(s) found`,
-      annotations: annotations.slice(0, 50) // TODO: FR what happens with more than 50?
-    }
-  }
+  return { conclusion, output: results.output }
 }
 
 async function run () {
@@ -170,9 +89,9 @@ async function run () {
   let report = {}
   try {
     process.chdir(GITHUB_WORKSPACE)
-    await installEslintPackagesAsync()
-    await setupEslintVersionAndLinter()
-    report = await runEslint()
+    await installJSPackagesAsync()
+    await setupTypescriptCommand()
+    report = await runTypeScriptCommand()
   } catch (e) {
     report = {
       conclusion: 'failure',

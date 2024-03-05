4c4eae767bb777d8021d0fd34ee78ed638caee52
GitHub Action <action@github.com>
2024-03-05 00:27:20 +0000

    Automated build of f594ce94462ade8cbd2400ef679b1208c7fc3ea4

--- dist/index.js
+++ dist/index.js
@@ -13785,13 +13785,15 @@ __nccwpck_require__.r(__webpack_exports__);
 /* harmony import */ var _ClientBase__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(3570);
 
 
-const { GITHUB_WORKSPACE } = process.env
+const { GITHUB_WORKSPACE, INPUT_TSCONFIG } = process.env
 
 const ERROR_REGEX = /(.*\.tsx?)[\(:](.*)[:,].*(error.*)/
 
 class TSCClient extends _ClientBase__WEBPACK_IMPORTED_MODULE_0__/* .ClientBase */ .K {
   get command() {
-    return `${GITHUB_WORKSPACE}/node_modules/.bin/tsc --pretty false`
+    return `${GITHUB_WORKSPACE}/node_modules/.bin/tsc --pretty false${
+      INPUT_TSCONFIG ? ` -p ${GITHUB_WORKSPACE}/${INPUT_TSCONFIG}` : ""
+    }`
   }
 
   get annotations() {
